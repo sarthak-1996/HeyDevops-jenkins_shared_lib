f927ec649e703e5538728f415bd1de1a05848941
Itsme <93462030+sarthak-1996@users.noreply.github.com>
2024-03-04 11:34:38 +0530

    used parameter of jfrog ip

--- vars/putArticatory.groovy
+++ vars/putArticatory.groovy
@@ -1,8 +1,8 @@
-def call(){
+def call(String jfrogip){
   withCredentials([usernamePassword(
             credentialsId: "jfrog",
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )])
-  sh 'curl -X PUT -u '$USER':'$PASS' -T /target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://23.20.203.179:8082/artifactory/example-repo-local/'
+  sh 'curl -X PUT -u '$USER':'$PASS' -T /target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${jfrogip}:8082/artifactory/example-repo-local/'
 }
