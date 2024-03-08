cc87442f1fa40e929a3fade2dca18da8f29dac0d
Itsme <93462030+sarthak-1996@users.noreply.github.com>
2024-03-08 12:25:42 +0530

    remove credential section

--- vars/putArticatory.groovy
+++ vars/putArticatory.groovy
@@ -1,8 +1,8 @@
 def call(String jfrogip){
-  withCredentials([usernamePassword(
-            credentialsId: "jfrog",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )])
-  sh 'curl -X PUT -u '$USER':'$PASS' -T /target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${jfrogip}:8082/artifactory/example-repo-local/'
+  // withCredentials([usernamePassword(
+  //           credentialsId: "jfrog",
+  //           usernameVariable: "USER",
+  //           passwordVariable: "PASS"
+  //   )])
+  sh 'curl -X PUT -u admin:Kolkata@01 -T /target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${jfrogip}:8082/artifactory/example-repo-local/'
 }
