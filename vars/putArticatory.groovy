c70bcea7451c13be41bf222835294990436760b8
Itsme <93462030+sarthak-1996@users.noreply.github.com>
2024-03-08 13:41:43 +0530

    Update putArticatory.groovy

--- vars/putArticatory.groovy
+++ vars/putArticatory.groovy
@@ -3,6 +3,8 @@ def call(String jfrogip){
             credentialsId: "jfrog",
             usernameVariable: "USER",
             passwordVariable: "PASS"
-    )])
-  sh "curl -X PUT -u ${USER}:${PASS} -T /var/jenkins/workspace/project/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${jfrogip}:8082/artifactory/example-repo-local/"
+    )]){
+        sh "curl -X PUT -u ${USER}:${PASS} -T /var/jenkins/workspace/project/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${jfrogip}:8082/artifactory/example-repo-local/"    
+    }
+  
 }
